7bd6fa0a67e12e82997faaae7f6da3c798885dd3
Pedro Sbardelotto <72877921+PedroSbardelotto@users.noreply.github.com>
2025-07-05 21:58:14 -0300

    cors implementado

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,10 @@
 import express from 'express';
-import router from './routes'; 
+import cors from 'cors';
+import router from './routes';
 import 'dotenv/config';
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
-
-
 app.use('/api', router);
-
 export default app;
